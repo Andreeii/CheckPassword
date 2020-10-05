48f08c261572b3a6bb2afaf9a5c7419069250a32
Turcanu <andriesturcanu@gmail.com>
2020-10-05 20:13:32 +0300

    Add project files.
    Small changes
    
    Initial commit2

---
+++ CheckPassword/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace CheckPassword
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+        string password = null;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.password = random.Next(1000, 10000).ToString();
+            try
+            {
+                string email = textEmail.Text;
+                Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                Match match = regex.Match(email);
+                if (match.Success)
+                {
+                    SmtpClient client = new SmtpClient();
+                    client.Host = "smtp.gmail.com";
+                    client.Port = 587;
+                    client.EnableSsl = true;
+                    client.Timeout = 10000;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.UseDefaultCredentials = true;
+                    client.Credentials = new NetworkCredential("ext999plus@gmail.com", "pass***!");
+
+                    MailMessage message = new MailMessage();
+                    message.From = new MailAddress("ext999plus@gmail.com");
+                    message.Subject = "Your one time password!";
+                    message.Body = password;
+                    message.To.Add(email);
+                    client.Send(message);
+                    MessageBox.Show("The password was sent");
+                }
+                else
+                {
+                    MessageBox.Show("The email email is not valid!");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (textPassword.Text == this.password)
+            {
+                MessageBox.Show("Success!");
+                this.password = null;
+            }
+            else
+            {
+                MessageBox.Show("Wrong password");
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
